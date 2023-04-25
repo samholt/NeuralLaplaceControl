b129f40d3b599bcaff79732d94ae3da9f7b1c368
Evgeny Saveliev <e.s.saveliev@gmail.com>
2023-04-25 13:47:31 +0000

    Add some comments to install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# Create a virtual environment with Python 3.9 (here using conda):
 conda create --name nlc python=3.9.7
 conda activate nlc
+
+# Install dependencies:
 conda install pytorch torchvision pytorch-cuda=11.6 -c pytorch -c nvidia
 pip install -r requirements.txt
+
+# If you have any issues with ffmpeg, try:
 # conda update ffmpeg
 # pip install imageio-ffmpeg
+
+# Optional. For library development, install developement dependencies.
+pip install -r requirements-dev.txt
